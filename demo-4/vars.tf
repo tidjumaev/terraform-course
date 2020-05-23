3ce81522d4efb936a717154cb4daae1a9d6a77eb
Timur Djumaev <timurdjumaev@TiMac.local>
2020-05-23 11:35:56 -0400

    some changes

--- demo-4/vars.tf
+++ demo-4/vars.tf
@@ -5,9 +5,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1 = "ami-039a49e70ea773ffc"
+    us-west-2 = "ami-003634241a8fcdec0"
+    eu-west-1 = "ami-0701e7be9b2a77600"
   }
 }
 
