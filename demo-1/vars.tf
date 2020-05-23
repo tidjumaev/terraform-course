898ea97cb269d31862ba629325065f7105842cf6
Timur Djumaev <timurdjumaev@TiMac.local>
2020-05-22 20:12:59 -0400

    some changes

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,19 +1,14 @@
-variable "AWS_ACCESS_KEY" {
-}
-
-variable "AWS_SECRET_KEY" {
-}
-
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
-
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-01d025118d8e760db"
+    us-west-2 = "ami-086b16d6badeb5716"
+    eu-west-1 = "ami-0b4b2d87bdd32212a"
   }
 }
 
